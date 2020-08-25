51af92f97f40eeae23dcf42424dbd7680186c90f
tomislav-ficko <ficko.tomislav@gmail.com>
2020-08-25 14:50:57 +0200

    link layout buttons with code

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -2,6 +2,9 @@ package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +12,8 @@ public class MainActivity extends Activity {
 
 
     // TODO: Declare member variables here:
+    private Button mTrueButton;
+    private Button mFalseButton;
 
 
     // TODO: Uncomment to create question bank
@@ -33,7 +38,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
+    public void trueButtonPressed(View view) {
+        Toast.makeText(getApplicationContext(), "True was selected!", Toast.LENGTH_SHORT).show();
+    }
 
+    public void falseButtonPressed(View view) {
+        Toast.makeText(getApplicationContext(), "False was selected!", Toast.LENGTH_SHORT).show();
     }
 }
