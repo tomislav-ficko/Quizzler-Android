5cc354aa24a663845564209da261c16c5f21984f
tomislav-ficko <ficko.tomislav@gmail.com>
2020-08-25 15:12:25 +0200

    add functionality to check answers

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -14,8 +15,12 @@ public class MainActivity extends Activity {
     // TODO: Declare member variables here:
     private Button mTrueButton;
     private Button mFalseButton;
+    private TextView mQuestionTextView;
+    private int mIndex;
+    private Question mQuestion;
+    private int score = 0;
 
-    private Question[] mQuestionBank = new Question[] {
+    private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_1, true),
             new Question(R.string.question_2, true),
             new Question(R.string.question_3, true),
@@ -28,7 +33,7 @@ public class MainActivity extends Activity {
             new Question(R.string.question_10, true),
             new Question(R.string.question_11, false),
             new Question(R.string.question_12, false),
-            new Question(R.string.question_13,true)
+            new Question(R.string.question_13, true)
     };
 
     @Override
@@ -36,13 +41,36 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        mQuestion = mQuestionBank[mIndex];
+        mQuestionTextView.setText(mQuestion.getQuestionId());
+
     }
 
     public void trueButtonPressed(View view) {
-        Toast.makeText(getApplicationContext(), "True was selected!", Toast.LENGTH_SHORT).show();
+        checkAnswer(true);
+        updateQuestion();
     }
 
     public void falseButtonPressed(View view) {
-        Toast.makeText(getApplicationContext(), "False was selected!", Toast.LENGTH_SHORT).show();
+        checkAnswer(false);
+        updateQuestion();
+    }
+
+    private void checkAnswer(boolean enteredAnswer) {
+        if (enteredAnswer == mQuestion.isAnswer()) {
+            Toast.makeText(getApplicationContext(), "Correct answer!", Toast.LENGTH_SHORT).show();
+            score++;
+        } else {
+            Toast.makeText(getApplicationContext(), "The answer was not correct!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void updateQuestion() {
+        mIndex = mIndex + 1;
+        mQuestion = mQuestionBank[mIndex];
+        mQuestionTextView.setText(mQuestion.getQuestionId());
     }
 }
